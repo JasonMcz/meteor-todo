de88ef0a08e419a4f4991efc9a316176ad407bb6
JasonMcz <bitcoin2095@gmail.com>
2017-03-30 17:05:16 -0400

    ADDED: User login, database schema and facebook connect

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,14 +1,35 @@
+
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
+
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
 });
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 
 Template.body.events({
   'submit .new-task'(event) {
@@ -22,10 +43,17 @@ Template.body.events({
     // Insert a task into the collection
     Tasks.insert({
       text,
-      createdAt: new Date(), // current time
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username, // current time
     });
+
+    // console.log
 
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
